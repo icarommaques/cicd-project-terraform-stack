73fae6bc9ad42dabe5d5cfbbdf868dfd6841bef9
Ubuntu <ubuntu@ip-172-31-89-224.ec2.internal>
2020-04-09 23:47:20 +0000

    atualizando buckets

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
- value = {
-    for instance in aws_instance.web:
-     instance.id => "http://${instance.public_dns}"
-  }
-}
-output "elb_public" {
-  value = "${aws_elb.web.dns_name}"
-}
--- state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "cicd-fiap-28ati-333050"
-    key    = "state/hackaton-cicd-deploy"
-    region = "us-east-1"
-  }
-}
